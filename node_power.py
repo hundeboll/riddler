54327106453f79c3f146ab3862152beb90194539
Martin Hundebøll <martin@hundeboll.net>
2012-03-13 09:14:12 +0100

    Increase serial timeout to 7 seconds on startup

--- node_power.py
+++ node_power.py
@@ -70,8 +70,9 @@ class power(threading.Thread):
 
         # Open device and wait for it to settle
         print("Open serial device: {}".format(self.args.power_dev))
-        self.ser = serial.Serial(self.args.power_dev, 9600, timeout=1)
-        time.sleep(5) # FIXME: Arduino need ~5sec to start serial
+        self.ser = serial.Serial(self.args.power_dev, 9600, timeout=7)
+        time.sleep(7) # FIXME: Arduino need ~5sec to start serial
+        self.ser.timeout = 1
         self.ser.flushInput()
         return True
 

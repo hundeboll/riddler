ac645056920dccb62d8aa59be623898a6c83ad9e
Martin Hundebøll <martin@hundeboll.net>
2013-06-27 21:05:51 +0200

    check recoding

--- riddler_node.py
+++ riddler_node.py
@@ -357,7 +357,7 @@ class node(threading.Thread):
             t = line.split(": ")
             key = "rlnc " + t[0]
             val = int(t[1])
-            if "recoder" is in key:
+            if "recoder" in key:
                 self.run_error = True
                 print("Sample error on {}: Started acting as recoder".format(self.name)
             obj.sample[key] = val

8ce7847e1d19c6f5e3a375121ebd4ccce6951980
Martin Hundebøll <martin@hundeboll.net>
2013-07-02 11:19:48 +0200

    add space to debug

--- node_server.py
+++ node_server.py
@@ -235,7 +235,7 @@ class tcp_handler(SocketServer.BaseRequestHandler):
         if self.run_info['coding'] in ("loss", "noloss"):
             return ""
 
-        print(" Sample fox")
+        print("  Sample fox")
         cmd = ["{}/tools/counters".format(os.path.dirname(self.server.args.fox_path))]
         p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
         p.wait()

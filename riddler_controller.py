895013459ce4da75f636c681b3108a3ad0687a8a
Martin Hundebøll <martin@hundeboll.net>
2013-07-18 15:08:47 +0200

    fix missing bracket

--- riddler_controller.py
+++ riddler_controller.py
@@ -345,7 +345,7 @@ class controller(threading.Thread):
         self.run_info['gen_size'] = self.args.gen_size
         self.run_info['packet_size'] = self.args.packet_size
         self.run_info['iperf_len'] = self.args.iperf_len
-        self.run_info['fixed_overshoot'] = self.args.fixed_overshoot[kwarg['coding']
+        self.run_info['fixed_overshoot'] = self.args.fixed_overshoot[kwarg['coding']]
         self.run_info['encoders'] = self.args.encoders
         self.run_info['encoder_timeout'] = self.args.encoder_timeout
         self.run_info['decoder_timeout'] = self.args.decoder_timeout
